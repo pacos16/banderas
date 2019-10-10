f3a2f72105fec00cfd355cfb80b73e557618b208
pacos16 <pacos16@gmail.com>
2019-10-10 19:07:03 +0200

    Funsiona

--- app/src/main/java/com/example/listviewcountries/CountryAdapter.java
+++ app/src/main/java/com/example/listviewcountries/CountryAdapter.java
@@ -2,9 +2,12 @@ package com.example.listviewcountries;
 
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,8 +24,20 @@ public class CountryAdapter extends ArrayAdapter {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
 
+        convertView= LayoutInflater.from(this.getContext()).inflate(R.layout.listitem_country,null);
+        Country country=countries[position];
+        TextView tvCountry= (TextView) convertView.findViewById(R.id.tvCountry);
+        tvCountry.setText(country.getCountryName());
+        TextView tvCapital= (TextView) convertView.findViewById(R.id.tvCapital);
+        tvCapital.setText(country.getCountryCapital());
+        TextView tvPopulation= (TextView) convertView.findViewById(R.id.tvPopulation);
+        tvPopulation.setText(""+country.getCountryPopulation());
+        ImageView ivFlag= (ImageView) convertView.findViewById(R.id.ivFlag);
+        int id = this.getContext().getResources().getIdentifier("_"+country.getCountryCode().toLowerCase(),
+                "drawable", ivFlag.getContext().getPackageName());
+        ivFlag.setImageResource(id);
+        return convertView;
 
     }
 }
--- app/src/main/java/com/example/listviewcountries/MainActivity.java
+++ app/src/main/java/com/example/listviewcountries/MainActivity.java
@@ -3,19 +3,24 @@ package com.example.listviewcountries;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
     private CountryParser countryParser;
     private CountryAdapter countryAdapter;
     private Country[] countries;
+    private ListView lvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lvMain= findViewById(R.id.lvMain);
         countryParser=new CountryParser(this);
         countryParser.parse();
         countries=countryParser.getCountries();
         countryAdapter=new CountryAdapter(this,countries);
+
+        lvMain.setAdapter(countryAdapter);
     }
 }
